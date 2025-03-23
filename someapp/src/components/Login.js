c5ef69931cc117ca2e760f705af2e079a0ed123c
gagan052 <147720611+gagan052@users.noreply.github.com>
2025-03-23 16:17:44 +0530

    Update Login.js

--- someapp/src/components/Login.js
+++ someapp/src/components/Login.js
@@ -39,7 +39,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:8080/api/auth/login', {
+      const response = await fetch('https://phonomania-backend.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
